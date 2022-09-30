50ea7c6878546d33f7f8530dbb902d7abd874799
Alexander McCormick <alexandermccormick@protonmail.com>
2022-09-29 18:04:06 -0700

    install and configure PurgeCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
+import htmlPurge from 'vite-plugin-purgecss'
 import vue from '@vitejs/plugin-vue'
+
+const htmlPurgeOptions = {
+  safelist: [
+    /-(leave|enter|appear)(|-(to|from|active))$/,
+    /^(?!(|.*?:)cursor-move).+-move$/,
+    /^router-link(|-exact)-active$/,
+    /data-v-.*/,
+    /leaflet-*/
+  ]
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    htmlPurge(htmlPurgeOptions) as PluginOption,
+    vue()
+  ],
+  build: {
+    minify: "terser"
+  },
   css: {
   preprocessorOptions: {
     scss: {
