6bee8e818d83c0bfabdacbec0bb68fcb5d26d891
Alexander McCormick <alexandermccormick@protonmail.com>
2022-10-17 19:47:41 -0700

    corrects vue-router alias for ghpages

--- src/modules/Router.ts
+++ src/modules/Router.ts
@@ -4,12 +4,16 @@ import MonitorMapVue from "../views/MonitorMap.vue";
 
 const MonitorDetailsVue = lazyLoad();
 
+const rootAlias = import.meta.env.VITE_BUILD_MODE === "ghp"
+  ? "/monitor-map"
+  : "/widget";
+
 export const RouterModule = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: "/",
-      alias: "/widget",
+      alias: rootAlias,
       name: "map",
       component: MonitorMapVue,
       children: [
