c8f49f0607debe29ede7887818a530649a8f99d5
Alexander McCormick <alexandermccormick@protonmail.com>
2022-10-20 23:07:43 -0700

    updates monitor reload duration to 1 hour

--- src/Map/Map.vue
+++ src/Map/Map.vue
@@ -30,7 +30,7 @@
 
     window.requestAnimationFrame(() => mapTarget.value!.appendChild(mapContainer));
 
-    monitorUpdateInterval = window.setInterval(async () => await loadMonitors(), 1000 * 60 * 2);
+    monitorUpdateInterval = window.setInterval(async () => await loadMonitors(), 1000 * 60 * 60);
 
     window.dispatchEvent(event);
   });
