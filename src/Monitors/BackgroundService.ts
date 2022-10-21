58da0267354a0362a1d6ada427448c4596e25434
Alexander McCormick <alexandermccormick@protonmail.com>
2022-10-21 15:35:08 -0700

    initial monitor search

--- src/Monitors/BackgroundService.ts
+++ src/Monitors/BackgroundService.ts
@@ -1,10 +1,11 @@
-import { ref } from "vue";
+import { ref, toRaw } from "vue";
 import { WorkerServiceClient } from "../Webworkers/WorkerServiceClient";
 import MonitorsWorkerService from "./worker?worker";
 
 import { Monitor } from "./Monitor";
 import type { DateRange } from "../models";
 import type { IMonitorEntry, IMonitorSubscription, MonitorId } from "../types";
+import {http} from "../modules";
 
 type MonitorsServiceModule = typeof import("./service");
 
@@ -35,4 +36,25 @@ export async function fetchSubscriptions(): Promise<Array<IMonitorSubscription>>
 
 export async function fetchTempByCoords(coords: [number, number]): Promise<number> {
   return await monitorsWorkerService.run("fetchTempByCoords", coords);
+}
+
+export function queryMonitorsData(queryString: string) {
+  queryString = queryString.toLowerCase();
+  
+  const externalResults = ref([]);
+
+  http(`https://api.maptiler.com/geocoding/${ queryString }.json?key=${ import.meta.env.VITE_MAPTILER_KEY }`)
+    .then(res => externalResults.value = res.data.features
+          .sort((a: any, b: any) => b.relevance - a.relevance)
+          .slice(0, 3))
+    .catch(() => console.log("Failed to fetch locations"));
+
+  const localResults = Object.values(monitors.value).map(m => m.data)
+    .filter(m => queryString.split(",").forEach(query => Object.values(m)
+            .filter(v => typeof v === "string")
+            .map(str => str.toLowerCase())
+            .some(val => val.includes(query.trim().toLowerCase()))));
+
+
+  return { localResults, externalResults };
 }
