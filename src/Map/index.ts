1de2e741e60e6fa7c26e803365c8343d3aa05916
Alexander McCormick <alexandermccormick@protonmail.com>
2022-11-14 15:57:55 -0800

    extracts monitor marker logic to DisplayOptions module and refactors into Vue Composables

--- src/Map/index.ts
+++ src/Map/index.ts
@@ -1,6 +1,6 @@
 export { default as MapVue} from "./Map.vue";
 export { default as MarkerLegendVue } from "./MarkerLegend.vue";
-export { evStationMarkersGroup, $mapTileSets, $overlayTilesets } from "./InteractiveMap";
+export * from "./InteractiveMap";
+export * from "./Tilesets";
 export { OverlayTileSet } from "./OverlayManagement";
 export { BaseTileset } from "./BaseLayerManagement";
-export * from "./mod";
