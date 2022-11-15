4a5f8e64a2e9019c6d41caab772dd558752f5da1
Alexander McCormick <alexandermccormick@protonmail.com>
2022-11-15 14:38:10 -0800

    updates vite-plugin-purgecss options

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ import dns from 'node:dns';
 dns.setDefaultResultOrder('ipv4first');
 
 const htmlPurgeOptions = {
+  content: [ `./public/**/*.html`, `./src/**/*.vue`, `./src/**/*.ts` ],
+  defaultExtractor (content: any) {
+    const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+    return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+  },
   safelist: [
     /-(leave|enter|appear)(|-(to|from|active))$/,
     /^(?!(|.*?:)cursor-move).+-move$/,
