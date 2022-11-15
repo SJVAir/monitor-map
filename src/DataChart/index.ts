ad02b6ada7eea93aaa33ea2af948379545a0c3d7
Alexander McCormick <alexandermccormick@protonmail.com>
2022-11-14 16:01:58 -0800

    updates exports

--- src/DataChart/index.ts
+++ src/DataChart/index.ts
@@ -2,5 +2,4 @@ export { default as DataChartVue } from "./DataChart.vue";
 export * from "./ChartDataPoint";
 export * from "./mod";
 export * from "./tooltip";
-export * from "./BackgroundService";
-export * as DataChartService from "./service";
+export * from "./service";
