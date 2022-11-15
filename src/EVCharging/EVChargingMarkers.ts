3ab49c4f66ebdbf78118009f2e94207d8af4d91e
Alexander McCormick <alexandermccormick@protonmail.com>
2022-11-15 13:47:15 -0800

    creates view for EV station marker management

---
+++ src/EVCharging/EVChargingMarkers.ts
@@ -0,0 +1,64 @@
+import { genEvStationMapMarker, useEVChargingService } from ".";
+import type { IEvStation } from "../types";
+import type { Ref } from "vue";
+
+//@ts-ignore: markerClusterGroup does not exist
+export const evStationMarkersGroup = L.markerClusterGroup({ clusterPane: "evStations" });
+
+let initialized = false;
+
+export async function useEVChangingMarkers(map?: L.Map) {
+  if (!initialized && map) {
+    initializeEVChargingMarkers(map);
+  }
+
+  const { fetchLvl2Stations, fetchLvl3Stations, lvl2EVStations, lvl3EVStations } = await useEVChargingService();
+
+  async function updateLvl2EvStations(ev: Event) {
+    updateEvStations(ev, lvl2EVStations, fetchLvl2Stations);
+  }
+
+  async function updateLvl3EvStations(ev: Event) {
+    updateEvStations(ev, lvl3EVStations, fetchLvl3Stations);
+  }
+
+  const lvl2 = {
+    label: "Level 2",
+    lvl2EVStations,
+    update: updateLvl2EvStations
+  }
+
+  const lvl3 = {
+    label: "Level 3",
+    lvl3EVStations,
+    update: updateLvl3EvStations
+  }
+
+  return [lvl2, lvl3];
+}
+
+function initializeEVChargingMarkers(map: L.Map) {
+  map.createPane("evStations").style.zIndex = "605";
+  evStationMarkersGroup.addTo(map);
+  initialized = true;
+}
+
+async function updateEvStations(
+  ev: Event,
+  collection: Ref<Array<IEvStation>>,
+  request: () => Promise<void>
+){
+  if ((ev.target as HTMLInputElement).checked) {
+
+    if (!collection.value.length) {
+      await request();
+    }
+
+    for (let station of collection.value) {
+      evStationMarkersGroup.addLayer(genEvStationMapMarker(station));
+    }
+
+  } else {
+    evStationMarkersGroup.clearLayers();
+  }
+}
---
+++ src/EVCharging/EVChargingMarkersManager.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+  import { useEVChangingMarkers } from './EVChargingMarkers';
+
+  const evChargingMarkers = await useEVChangingMarkers();
+</script>
+
+<template>
+  <p class="display-group-label">EV Stations</p>
+  <div v-for="stationType in evChargingMarkers" class="dropdown-item">
+    <label class="checkbox">
+      <input type="checkbox" @change.preventDefault="stationType.update" />
+      <span class="icon ev-icon">
+        <span class="material-symbols-outlined">
+          ev_station
+        </span>
+      </span>
+      <span class="option-label has-text-black">
+        {{ stationType.label }}
+      </span>
+    </label>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  @use "sass:color";
+
+  .icon.ev-icon {
+    @extend .has-text-white;
+    margin-left: 3px;
+    background-color: $sjvair-main;
+    border: 2px solid color.scale($sjvair-main, $lightness: -20%);
+    border-radius: 50%;
+    width: 18px;
+    height: 18px;
+
+    .material-symbols-outlined {
+      font-size: 14px;
+      max-width: 14px;
+
+      font-variation-settings:
+      'FILL' 0,
+    }
+  }
+</style>
