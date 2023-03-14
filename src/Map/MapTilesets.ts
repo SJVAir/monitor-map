16351f82e1423ce64c99da9aac797cdd53e449b5
Alexander McCormick <alexandermccormick@protonmail.com>
2023-03-14 09:46:49 -0700

    updates MapTilesets to use asyncInitializer and DisplayOption types

--- src/Map/MapTilesets.ts
+++ src/Map/MapTilesets.ts
@@ -1,11 +1,15 @@
+import { ref, watch } from "vue";
+import L from "../modules/Leaflet";
 import { useInteractiveMap } from "./InteractiveMap";
-import { MapTileset } from "./MapTileset";
-import type { MapTilesetCollection } from "../types";
+import { activeOverlays } from "./OverlayTileset";
+import { asyncInitializer } from "../modules";
+import type { TileLayerDisplayOptions } from "../DisplayOptions";
 
-const tilesets: MapTilesetCollection = [
-  {
+const mapTilesets: TileLayerDisplayOptions = { 
+  streets: {
     label: "Streets",
     isDefault: true,
+    model: ref(true),
     svg: "road-svg",
     urlTemplate: "https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key={apiKey}",
     options: {
@@ -14,10 +18,13 @@ const tilesets: MapTilesetCollection = [
       attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
     }
   },
-  {
+  satellite: {
     label: "Satellite Hybrid",
+    model: ref(false),
     urlTemplate: "https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key={apiKey}",
-    icon: "satellite_alt",
+    icon: {
+      id: "satellite_alt"
+    },
     containerClass: "has-text-silver",
     options: {
       maxZoom: 19,
@@ -25,9 +32,12 @@ const tilesets: MapTilesetCollection = [
       attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
     }
   },
-  {
+  topographique: {
     label: "Topographique",
-    icon: "landscape",
+    model: ref(false),
+    icon: {
+      id: "landscape"
+    },
     containerClass: "has-text-success",
     urlTemplate: "https://api.maptiler.com/maps/topographique/{z}/{x}/{y}.png?key={apiKey}",
     options: {
@@ -36,24 +46,41 @@ const tilesets: MapTilesetCollection = [
       attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
     }
   }
-];
+};
 
-let mapTileSets: Array<MapTileset>;
-let initialized = false;
+export const useMapTilesets = asyncInitializer<TileLayerDisplayOptions>((resolve, reject) => {
+  useInteractiveMap()
+    .then(({ map }) => {
+      const sources = Object.values(mapTilesets).map(ts => () => ts.model.value);
+      const { streets } = mapTilesets;
+      let currentTileset = streets.label;
+      let baseTileset = L.tileLayer(streets.urlTemplate, streets.options).addTo(map);
 
-export async function useMapTilesets() {
-  if (!initialized) {
-    await initializeTilesets();
-  }
-  return mapTileSets;
-}
+      watch(
+        sources,
+        (isChecked) => {
+          console.log("running watcher")
+          window.requestAnimationFrame(() => {
+            if (isChecked) {
+              Object.values(mapTilesets).find(ts => ts.label === currentTileset)!.model.value = false;
+              baseTileset.remove();
 
-async function initializeTilesets() {
-  const { map } = await useInteractiveMap();
+              const tileset = Object.values(mapTilesets).find(ts => ts.model.value === true)!;
+              console.log("tileset: ", tileset)
+              console.log("maptTilesets: ", mapTilesets);
+              baseTileset = L.tileLayer(tileset.urlTemplate, tileset.options).addTo(map);
 
-  mapTileSets = tilesets.map(mts => new MapTileset(map, mts));
-  
-  mapTileSets.find(ts => ts.isDefault)?.enable();
+              if (activeOverlays.size) {
+                for (let layer of activeOverlays.values()) {
+                  layer.redraw();
+                }
+              }
+            }
+          });
+        }
+      );
 
-  initialized = true;
-}
+      resolve(mapTilesets);
+    })
+    .catch(reject);
+});
--- src/Map/MapTilesetsManager.vue
+++ src/Map/MapTilesetsManager.vue
@@ -9,10 +9,10 @@
     <p class="display-group-label">Map Tiles</p>
     <div v-for="tileset in mapTilesets" class="dropdown-item" :class="tileset.containerClass">
       <label class="radio">
-        <input type="radio" :checked="tileset.isDefault" name="tiles" @click="() => tileset.enable()"/>
+        <input type="radio" :checked="tileset.isChecked.value" v-model="tileset.isChecked.value" name="tiles" />
         <span v-if="tileset.icon" class="icon">
           <span class="material-symbols-outlined">
-            {{ tileset.icon }}
+            {{ tileset.icon.id }}
           </span>
         </span>
         <span v-else="tileset.svg" class="icon" :class="tileset.svg"></span>
