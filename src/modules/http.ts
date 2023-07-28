765d8d04c26f4be2e84d63353eaa894063962cde
Alexander McCormick <alexandermccormick@protonmail.com>
2023-07-28 14:10:30 -0700

    updates generation of base url for requests

--- src/modules/http.ts
+++ src/modules/http.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
-const baseURL = (import.meta.env.VITE_REQUEST_MODE !== "leach")
-  ? "/api/1.0/"
-  : "https://www.sjvair.com/api/1.0/";
+const baseURL = (import.meta.env.PROD)
+  ? new URL(import.meta.url).origin
+  : "/api/1.0/";
 
 export const http = Axios.create({ baseURL });
