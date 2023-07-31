eb912c0dd016c5fc4b7cbd5eac57735d9013dce0
Alexander McCormick <alexandermccormick@protonmail.com>
2023-07-31 15:43:56 -0700

    corrects production api url

--- src/modules/http.ts
+++ src/modules/http.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const baseURL = (import.meta.env.PROD)
-  ? new URL(import.meta.url).origin
+  ? `${new URL(import.meta.url).origin}/api/1.0/`
   : "/api/1.0/";
 
 export const http = Axios.create({ baseURL });
