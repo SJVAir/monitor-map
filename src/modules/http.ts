b318a2ecac218cf524271b98dcd24c4df93e64c0
Alexander McCormick <alexandermccormick@protonmail.com>
2023-08-10 14:43:55 -0700

    corrects generation of base url in web workers

--- src/modules/http.ts
+++ src/modules/http.ts
@@ -1,7 +1,16 @@
 import Axios from 'axios';
 
-const baseURL = (import.meta.env.PROD)
-  ? `${window.location.origin}/api/1.0/`
-  : "/api/1.0/";
+const baseURL = (() => {
+  if (import.meta.env.PROD) {
+    if (window !== undefined) {
+      return window.location.origin;
+    } else {
+      return self.location.origin
+    }
+  } else {
+    return "/api/1.0/";
+  }
+})();
+
 
 export const http = Axios.create({ baseURL });
