bbf6517e99cdeaf26dd7fb2a114174f8c0dbc127
Alexander McCormick <alexandermccormick@protonmail.com>
2025-03-12 15:35:02 -0700

    reverts http module

--- src/modules/http.ts
+++ src/modules/http.ts
@@ -1,15 +1,15 @@
 import Axios from "axios";
 
-//const baseURL = (() => {
-//  if (import.meta.env.PROD) {
-//    const origin = (typeof window !== "undefined")
-//      ? window.location.origin
-//      : self.location.origin
-//
-//    return `${ origin }/api/1.0/`
-//  } else {
-//    return "/api/1.0/";
-//  }
-//})();
+const baseURL = (() => {
+  if (import.meta.env.PROD) {
+    const origin = (typeof window !== "undefined")
+      ? window.location.origin
+      : self.location.origin
 
-export const http = Axios.create({ baseURL: "https://www.sjvair.com/api/1.0/" });
+    return `${origin}/api/1.0/`
+  } else {
+    return "/api/1.0/";
+  }
+})();
+
+export const http = Axios.create({ baseURL });
