3fd0f939c6864d729b8e895d662ca944eb6b4b70
Alexander McCormick <alexandermccormick@protonmail.com>
2025-06-11 16:58:30 -0700

    Creates new type to bypass missing toJSON methods

--- src/modules/location.ts
+++ src/modules/location.ts
@@ -6,9 +6,13 @@ export const defaultCoordinates = {
   longitude: -119.7989545249089,
 };
 
+interface GeolocationPoint extends Omit<GeolocationPosition, "coords" | "toJSON"> {
+  coords: Omit<GeolocationPosition["coords"], "toJSON">
+}
+
 export async function getCurrentPosition(
   options?: PositionOptions,
-): Promise<GeolocationPosition> {
+): Promise<GeolocationPoint> {
   return new Promise((resolve) => {
     if (hasGeolocation) {
       return navigator.geolocation.getCurrentPosition(resolve, console.error, options);
