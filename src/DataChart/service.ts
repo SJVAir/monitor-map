5ceae3483097a79fb4feab252e4ad3f60ace7db9
Alexander McCormick <alexandermccormick@protonmail.com>
2025-07-14 15:57:36 -0700

    removes old dynamic module resolution

--- src/DataChart/service.ts
+++ src/DataChart/service.ts
@@ -4,13 +4,13 @@ interface DataChartServiceConfig {
 }
 
 export async function useDataChartService(config?: DataChartServiceConfig) {
-  const opts: DataChartServiceConfig = {
-    webworker: config?.webworker || true
-  };
+  //const opts: DataChartServiceConfig = {
+  //  webworker: config?.webworker || true
+  //};
 
-  const service = (opts.webworker)
-    ? await import("./BackgroundRequests")
-    : await import("./requests");
+  //const service = (opts.webworker)
+  //  ? await import("./BackgroundRequests")
+  //  : await import("./requests");
 
 
   return { fetchChartData };
