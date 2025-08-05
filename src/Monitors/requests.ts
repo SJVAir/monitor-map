55656f86a0cd3f8dbb11d31ef7c64be15d1eb4c2
Alexander McCormick <alexandermccormick@protonmail.com>
2025-08-04 17:17:51 -0700

    Temp fix: exclude monitors with null positions

--- src/Monitors/requests.ts
+++ src/Monitors/requests.ts
@@ -16,7 +16,10 @@ export async function fetchMonitors(pollutant: "pm25" | "o3"): Promise<MonitorsR
 
       if (monitors && Object.keys(monitors).length) {
         for (let monitorData of monitors) {
-          record[monitorData.id] = new Monitor(monitorData);
+          // This if statement can be removed once the api has been tested
+          if (monitorData.position !== null) {
+            record[monitorData.id] = new Monitor(monitorData);
+          }
         }
       }
 
