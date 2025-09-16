30d2c9b5a52b93ee91f7b9c8dd99c3c255e22cd9
Alexander McCormick <alexandermccormick@protonmail.com>
2025-09-16 14:34:48 -0700

    removes debug objects

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,3 @@
 import { initialize } from "./App";
 
-(window as any).USER = {
-  id: "NFZ2MmtAQLmzyaZPBMquNQ",
-  name: "Alexander McCormick",
-  email: "a.mccormick989@gmail.com",
-  is_authenticated: true,
-  is_anonymous: false
-};
-
 initialize();
