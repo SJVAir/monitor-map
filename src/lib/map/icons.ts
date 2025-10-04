9292ca24718692eb7993ffb8ae7a9e6789a6df26
Alexander McCormick <alexandermccormick@protonmail.com>
2025-10-03 20:31:24 -0700

    Adds icons and render ordering

--- src/lib/map/icons.ts
+++ src/lib/map/icons.ts
@@ -1,16 +1,32 @@
 import { darken } from "color2k";
 
-export function square(color: string) {
+export function squareSVG(color: string, borderWidth: number) {
   return `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-  <rect x="2" y="2" width="20" height="20" fill="${darken(color, 0.1)}"/>
-  <rect x="4" y="4" width="16" height="16" fill="${color}"/>
-</svg>`
+    <rect x="${borderWidth / 2}" y="${borderWidth / 2}" width="${24 - borderWidth}" height="${24 - borderWidth}"
+      fill="${color}" stroke="${darken(color, 0.1)}" stroke-width="${borderWidth}"/>
+  </svg>`;
 }
 
-export function zquare(color: string) {
-  return `data:image/svg+xml;utf8,<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><rect x="2" y="2" width="20" height="20" fill="${darken(color, 0.1)}"/><rect x="4" y="4" width="16" height="16" fill="${color}"/></svg>`;
+export function circleSVG(color: string, borderWidth: number) {
+  return `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="${12 - borderWidth}" fill="${color}" stroke="${darken(color, 0.1)}" stroke-width="${borderWidth}"/>
+  </svg>`;
 }
+
+export function triangleSVG(color: string, borderWidth: number) {
+  const points = `${12},${borderWidth} ${24 - borderWidth},${24 - borderWidth} ${borderWidth},${24 - borderWidth}`;
+  return `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <polygon points="${points}"
+      fill="${color}" stroke="${darken(color, 0.1)}" stroke-width="${borderWidth}"/>
+  </svg>`;
+}
+
 export const Icons = {
-  square: (color: string) => `data:image/svg+xml;utf8,${encodeURIComponent(square(color))}`
+  square: (color: string, borderWidth: number): string =>
+    `data:image/svg+xml;utf8,${encodeURIComponent(squareSVG(color, borderWidth))}`,
+  circle: (color: string, borderWidth: number): string =>
+    `data:image/svg+xml;utf8,${encodeURIComponent(circleSVG(color, borderWidth))}`,
+  triangle: (color: string, borderWidth: number): string =>
+    `data:image/svg+xml;utf8,${encodeURIComponent(triangleSVG(color, borderWidth))}`
 };
 
--- src/lib/map/map.svelte.ts
+++ src/lib/map/map.svelte.ts
@@ -1,7 +1,7 @@
 import { config, Map, MapStyle } from "@maptiler/sdk";
 import { Singleton } from "@tstk/decorators";
-import type { Feature, GeoJSON, Geometry } from "geojson";
-import { darken } from "color2k";
+import type { Feature, Geometry } from "geojson";
+//import { darken } from "color2k";
 import { MonitorsController } from "$lib/monitors/monitors.svelte";
 import { LoadingQueue } from "$lib/load-screen/load-screen.svelte.ts";
 import { Initializer } from "$lib/decorators/initializer.ts";
@@ -10,8 +10,9 @@ import type { MonitorData, SJVAirEntryLevel } from "@sjvair/sdk";
 //import { WindLayer } from "@maptiler/weather";
 
 interface MonitorMarkerProperties {
-  valueColor: string;
-  borderColor: string;
+  //valueColor: string;
+  //borderColor: string;
+  order: number;
   icon: string;
 }
 
@@ -34,10 +35,11 @@ function getIcon<T extends MonitorData>(monitor: T): string {
 }
 
 function buildIcons(levels: Array<SJVAirEntryLevel> | null) {
+  const borderWidth = 2;
   const icons: Record<string, HTMLImageElement> = {};
 
   const greySquareIcon = new Image();
-  greySquareIcon.src = Icons.square(defaultColor);
+  greySquareIcon.src = Icons.square(defaultColor, 2);
   greySquareIcon.width = 24;
   greySquareIcon.height = 24;
 
@@ -45,15 +47,45 @@ function buildIcons(levels: Array<SJVAirEntryLevel> | null) {
 
   if (levels) {
     for (const level of levels) {
-      const icon = new Image();
-      icon.src = Icons.square(level.color);
-      icon.width = 24;
-      icon.height = 24;
-      icons[`${level.name}-square`] = icon;
+      for (const [shape, svg] of Object.entries(Icons)) {
+        shape
+        const icon = new Image();
+        icon.src = svg(level.color, borderWidth);
+        icon.width = 24;
+        icon.height = 24;
+        icons[`${level.name}-${shape}`] = icon;
+      }
+      //const icon = new Image();
+      //icon.src = Icons.square(level.color);
+      //icon.width = 24;
+      //icon.height = 24;
+      //icons[`${level.name}-square`] = icon;
     }
   }
 
   return icons;
+}
+
+function getOrder<T extends MonitorData>(monitor: T): number {
+  switch (monitor.type) {
+    case "airgradient":
+      return 5;
+
+    case "airnow":
+      return 2;
+
+    case "aqview":
+      return 3;
+
+    case "bam1022":
+      return 6;
+
+    case "purpleair":
+      return monitor.is_sjvair ? 4 : 1;
+
+    default:
+      throw new Error(`Map ordering for ${monitor.device} has not been set`);
+  }
 }
 
 const mapLoading = Symbol();
@@ -96,71 +128,135 @@ export class MapController {
       //  opacity: 0.5,
       //}); // using default settings
       //this.map!.addLayer(layer);
+      //for (const monitorMeta of mc.meta.asIter.monitors) {
+      //  const monitors = mc.latest.filter(m => m.type === monitorMeta.type)
 
-      for (const monitorMeta of mc.meta.asIter.monitors) {
-        const monitors = mc.latest.filter(m => m.type === monitorMeta.type)
+      //  // NOTE: Cast to "any" to avoid type conflicts with MapLibre and Zod types
+      //  const features: Array<Feature<Geometry, MonitorMarkerProperties>> = monitors.map(m => {
+      //    const feature: Feature<Geometry, MonitorMarkerProperties> = {
+      //      type: "Feature",
+      //      properties: {
+      //        valueColor: defaultColor,
+      //        borderColor: darken(defaultColor, 0.1),
+      //        //icon: getIcon(m)
+      //        icon: "default"
+      //      },
+      //      geometry: m.position! as Geometry
+      //    };
 
-        // NOTE: Cast to "any" to avoid type conflicts with MapLibre and Zod types
-        const features: Array<Feature<Geometry, MonitorMarkerProperties>> = monitors.map(m => {
-          const feature: Feature<Geometry, MonitorMarkerProperties> = {
-            type: "Feature",
-            properties: {
-              valueColor: defaultColor,
-              borderColor: darken(defaultColor, 0.1),
-              //icon: getIcon(m)
-              icon: "default"
-            },
-            geometry: m.position! as Geometry
-          };
+      //    if (levels) {
+      //      const level = levels.find(lvl => {
+      //        const value = parseInt(m.latest.value, 10);
+      //        return value >= lvl.range[0] && value <= lvl.range[1];
+      //      });
 
-          if (levels) {
-            const level = levels.find(lvl => {
-              const value = parseInt(m.latest.value, 10);
-              return value >= lvl.range[0] && value <= lvl.range[1];
-            });
-
-            if (level) {
-              feature.properties.valueColor = level.color;
-              feature.properties.borderColor = darken(level.color, 0.1);
-              feature.properties.icon = `${level.name}-square`;
-            }
-          }
+      //      if (level) {
+      //        feature.properties.valueColor = level.color;
+      //        feature.properties.borderColor = darken(level.color, 0.1);
+      //        feature.properties.icon = `${level.name}-${getIcon(m)}`;
+      //        //feature.properties.icon = getIcon(m);
+      //      }
+      //    }
 
 
-          return feature;
-        });
+      //    return feature;
+      //  });
 
-        this.map!.addSource(monitorMeta.type, {
-          type: "geojson",
-          data: {
-            type: "FeatureCollection",
-            features
-          }
-        });
+      //  this.map!.addSource(monitorMeta.type, {
+      //    type: "geojson",
+      //    data: {
+      //      type: "FeatureCollection",
+      //      features
+      //    }
+      //  });
 
-        this.map!.addLayer({
-          id: monitorMeta.type,
-          type: "symbol",
-          source: monitorMeta.type,
-          layout: {
-            "icon-allow-overlap": true,
-            "icon-ignore-placement": true,
-            "icon-image": ["get", "icon"],
-            //"icon-image": "grey-square",
-            "icon-size": 1
+      //  this.map!.addLayer({
+      //    id: monitorMeta.type,
+      //    type: "symbol",
+      //    source: monitorMeta.type,
+      //    layout: {
+      //      "icon-allow-overlap": true,
+      //      "icon-ignore-placement": true,
+      //      "icon-image": ["get", "icon"],
+      //      "icon-size": 1
+      //      //"icon-size": 1.5
+      //    },
+      //    paint: {
+      //      //"icon-color": ["get", "valueColor"],
+      //      //"icon-halo-color": ["get", "borderColor"],
+      //      //"icon-halo-width": 2.2,
+
+      //      //"circle-radius": 10,
+      //      //"circle-color": ["get", "valueColor"],
+      //      //"circle-stroke-width": 2,
+      //      //"circle-stroke-color": ["get", "borderColor"]
+      //    },
+      //  });
+      //}
+
+      // NOTE: Cast to "any" to avoid type conflicts with MapLibre and Zod types
+      const features: Array<Feature<Geometry, MonitorMarkerProperties>> = mc.latest.map(m => {
+        const feature: Feature<Geometry, MonitorMarkerProperties> = {
+          type: "Feature",
+          properties: {
+            //valueColor: defaultColor,
+            //borderColor: darken(defaultColor, 0.1),
+            //icon: getIcon(m)
+            order: getOrder(m),
+            icon: "default"
           },
-          paint: {
-            //"icon-color": ["get", "valueColor"],
-            //"icon-halo-color": ["get", "borderColor"],
-            //"icon-halo-width": 2.2
+          geometry: m.position! as Geometry
+        };
 
-            //"circle-radius": 10,
-            //"circle-color": ["get", "valueColor"],
-            //"circle-stroke-width": 2,
-            //"circle-stroke-color": ["get", "borderColor"]
-          },
-        });
-      }
+        if (levels) {
+          const level = levels.find(lvl => {
+            const value = parseInt(m.latest.value, 10);
+            return value >= lvl.range[0] && value <= lvl.range[1];
+          });
+
+          if (level) {
+            //feature.properties.valueColor = level.color;
+            //feature.properties.borderColor = darken(level.color, 0.1);
+            feature.properties.icon = `${level.name}-${getIcon(m)}`;
+            //feature.properties.icon = getIcon(m);
+          }
+        }
+
+
+        return feature;
+      });
+
+      this.map!.addSource("monitors", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features
+        }
+      });
+
+      this.map!.addLayer({
+        id: "monitors",
+        type: "symbol",
+        source: "monitors",
+        layout: {
+          "symbol-sort-key": ["get", "order"],
+          "icon-allow-overlap": true,
+          "icon-ignore-placement": true,
+          "icon-image": ["get", "icon"],
+          "icon-size": 1
+          //"icon-size": 1.5
+        },
+        paint: {
+          //"icon-color": ["get", "valueColor"],
+          //"icon-halo-color": ["get", "borderColor"],
+          //"icon-halo-width": 2.2,
+
+          //"circle-radius": 10,
+          //"circle-color": ["get", "valueColor"],
+          //"circle-stroke-width": 2,
+          //"circle-stroke-color": ["get", "borderColor"]
+        },
+      });
 
       lq.remove(mapLoading);
     });
