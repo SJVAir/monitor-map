136f76061b49c74d543a3be159571037713d4ca8
Alexander McCormick <alexandermccormick@protonmail.com>
2025-10-04 14:40:29 -0700

    Creates Derived decorator

--- src/lib/reactivity.svelte.ts
+++ src/lib/reactivity.svelte.ts
@@ -30,6 +30,35 @@ class ShallowReactiveState<T> {
   }
 }
 
+
+class DerivedReactiveState<T> {
+  private internalValue?: T;
+
+  constructor(expression: () => T) {
+    this.internalValue = $derived.by(expression);
+  }
+  get value(): T | undefined {
+    return this.internalValue;
+  }
+  set value(newValue: T) {
+    this.internalValue = newValue;
+  }
+}
+
+export function Derived<V>(value: () => V) {
+  let ref: DerivedReactiveState<V> = new DerivedReactiveState<V>(value);
+  return function <V, T = unknown>(
+    _target: ClassAccessorDecoratorTarget<T, V>,
+    _ctx: ClassAccessorDecoratorContext,
+  ): ClassAccessorDecoratorResult<T, V> {
+
+    return {
+      get() {
+        return ref.value as V;
+      },
+    };
+  }
+}
 export function Reactive(shallow?: boolean) {
   return function <V, T = unknown>(
     _target: ClassAccessorDecoratorTarget<T, V>,
@@ -38,10 +67,10 @@ export function Reactive(shallow?: boolean) {
     let ref = shallow ? new ShallowReactiveState<V>() : new ReactiveState<V>();
 
     return {
-      get() {
+      get(): V {
         return ref.value as V;
       },
-      set(newValue) {
+      set(newValue): void {
         ref.value = newValue;
       },
     };
