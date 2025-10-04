e2459177b89a9e71e4a8b1a63a2718b4c19dfe52
Alexander McCormick <alexandermccormick@protonmail.com>
2025-10-04 14:40:50 -0700

    Removes SVG from shape function names

--- src/lib/map/icons.ts
+++ src/lib/map/icons.ts
@@ -1,19 +1,20 @@
+import type { Map } from "@maptiler/sdk";
 import { darken } from "color2k";
 
-export function squareSVG(color: string, borderWidth: number) {
+export function square(color: string, borderWidth: number) {
   return `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <rect x="${borderWidth / 2}" y="${borderWidth / 2}" width="${24 - borderWidth}" height="${24 - borderWidth}"
       fill="${color}" stroke="${darken(color, 0.1)}" stroke-width="${borderWidth}"/>
   </svg>`;
 }
 
-export function circleSVG(color: string, borderWidth: number) {
+export function circle(color: string, borderWidth: number) {
   return `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <circle cx="12" cy="12" r="${12 - borderWidth}" fill="${color}" stroke="${darken(color, 0.1)}" stroke-width="${borderWidth}"/>
   </svg>`;
 }
 
-export function triangleSVG(color: string, borderWidth: number) {
+export function triangle(color: string, borderWidth: number) {
   const points = `${12},${borderWidth} ${24 - borderWidth},${24 - borderWidth} ${borderWidth},${24 - borderWidth}`;
   return `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <polygon points="${points}"
@@ -21,12 +22,15 @@ export function triangleSVG(color: string, borderWidth: number) {
   </svg>`;
 }
 
-export const Icons = {
-  square: (color: string, borderWidth: number): string =>
-    `data:image/svg+xml;utf8,${encodeURIComponent(squareSVG(color, borderWidth))}`,
-  circle: (color: string, borderWidth: number): string =>
-    `data:image/svg+xml;utf8,${encodeURIComponent(circleSVG(color, borderWidth))}`,
-  triangle: (color: string, borderWidth: number): string =>
-    `data:image/svg+xml;utf8,${encodeURIComponent(triangleSVG(color, borderWidth))}`
-};
+export function asDataURI(svg: string): string {
+  return `data:image/svg+xml;utf8,${encodeURIComponent(svg)}`;
+}
 
+//export const Icons = {
+//  square: (color: string, borderWidth: number): string =>
+//    `data:image/svg+xml;utf8,${encodeURIComponent(squareSVG(color, borderWidth))}`,
+//  circle: (color: string, borderWidth: number): string =>
+//    `data:image/svg+xml;utf8,${encodeURIComponent(circleSVG(color, borderWidth))}`,
+//  triangle: (color: string, borderWidth: number): string =>
+//    `data:image/svg+xml;utf8,${encodeURIComponent(triangleSVG(color, borderWidth))}`
+//};
