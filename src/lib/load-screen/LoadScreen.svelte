a93d892caf9f17fceb2ec851537ee56c89f32165
Alexander McCormick <alexandermccormick@protonmail.com>
2025-10-04 22:45:20 -0700

    Adds initial loading images

--- src/lib/load-screen/LoadScreen.svelte
+++ src/lib/load-screen/LoadScreen.svelte
@@ -5,9 +5,17 @@
 </script>
 
 {#if lq.loading}
-  <div class="absolute top-0 left-0 bg-sky-400 w-full h-full z-50 flex flex-col justify-center items-center gap-4">
-    <h1 class="text-lg">A cooler loading screen awaits....</h1>
-    <div class="h-16 w-16 rounded-full border-8 border-t-transparent border-blue-500 animate-spin"></div>
-  </div>
+	<div
+		class="absolute top-0 left-0 z-50 flex h-full w-full flex-col items-center justify-center gap-4 bg-sky-400"
+	>
+		<div>
+			<enhanced:img src="$lib/assets/sjvair-icon.webp" alt="An alt text" />
+			<enhanced:img src="$lib/assets/sjvair-sjv.webp" alt="An alt text" />
+			<enhanced:img src="$lib/assets/sjvair-air.webp" alt="An alt text" />
+		</div>
+		<h1 class="text-lg">A cooler loading screen awaits....</h1>
+		<div
+			class="h-16 w-16 animate-spin rounded-full border-8 border-blue-500 border-t-transparent"
+		></div>
+	</div>
 {/if}
-
