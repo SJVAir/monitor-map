b94478b0f1e6aacc8d8aac799d20f631a85661f0
Alexander McCormick <alexandermccormick@protonmail.com>
2025-10-15 16:51:43 -0700

    Creates new Wind module

---
+++ src/lib/wind/WindDisplayOptions.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { WindMapIntegration } from "./wind.svelte";
+
+	const wind = new WindMapIntegration();
+</script>
+
+<div class="flex-col items-center justify-center">
+  <p class="text-lg font-bold underline">Map Layers</p>
+  <label for="wind" class="m-2 cursor-pointer whitespace-nowrap select-none">
+    <input type="checkbox" id="wind" name="wind" bind:checked={wind.enabled} />
+    Wind
+  </label>
+</div>
