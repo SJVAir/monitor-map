e88fc95846974e7277189e08069f86e5c5823763
Alexander McCormick <alexandermccormick@protonmail.com>
2025-10-15 16:54:31 -0700

    Updates display options

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,14 @@
 	import Map from "$lib/map/Map.svelte";
 	import Menu from "$lib/map/Menu.svelte";
 	import LoadingScreen from "$lib/loading/screen/LoadScreen.svelte";
-	import { MonitorsMapIntegration } from "$lib/monitors/monitors-map-integration.svelte.ts";
+	import MonitorsDisplayOptions from "$lib/monitors/MonitorsDisplayOptions.svelte";
+	import WindDisplayOptions from "$lib/wind/WindDisplayOptions.svelte";
 	import { MonitorsController } from "$lib/monitors/monitors.svelte";
+	import { MonitorsMapIntegration } from "$lib/monitors/monitors-map-integration.svelte.ts";
+	import { WindMapIntegration } from "$lib/wind/wind.svelte";
 
 	const monitors = new MonitorsController();
-	const integrations = [new MonitorsMapIntegration()];
+	const integrations = [new WindMapIntegration(), new MonitorsMapIntegration()];
 
 	await monitors.init();
 
@@ -20,7 +23,10 @@
 	<LoadingScreen />
 	<Map {integrations} />
 	<div class="absolute top-0 left-0 z-10 m-4">
-		<Menu />
+		<Menu>
+			<MonitorsDisplayOptions />
+			<WindDisplayOptions />
+		</Menu>
 	</div>
 </main>
 
