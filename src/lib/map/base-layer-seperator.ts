233db1de9e6c3766aa179f3887d09d521cae485b
Alexander McCormick <alexandermccormick@protonmail.com>
2025-10-15 16:52:03 -0700

    Adds missing enabled property

--- src/lib/map/base-layer-seperator.ts
+++ src/lib/map/base-layer-seperator.ts
@@ -1,10 +1,12 @@
 import { Map as MaptilerMap } from "@maptiler/sdk";
 import { Singleton } from "@tstk/decorators";
-import type { MapIntegration } from "./integrations.svelte.ts";
+import type { MapIntegration } from "./integrations.ts";
 
 @Singleton
 export class BaseLayerSeperator implements MapIntegration {
   referenceId: string = "Base Layer Seperator"
+
+  enabled: boolean = true;
 
   mapLayer: Parameters<MaptilerMap["addLayer"]>[0] = {
     id: this.referenceId,
