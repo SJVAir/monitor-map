d9e3ac704075b662231d4f04db707ad7aea82273
Alexander McCormick <alexandermccormick@protonmail.com>
2025-10-15 16:54:23 -0700

    Updates display options to accept children

--- src/lib/map/Menu.svelte
+++ src/lib/map/Menu.svelte
@@ -1,36 +1,9 @@
 <script lang="ts">
-	import { MonitorsController } from "$lib/monitors/monitors.svelte";
-	import { WindMapIntegration } from "./wind.svelte";
-
-	const mc = new MonitorsController();
-	const wind = new WindMapIntegration();
+	let { children } = $props();
 </script>
 
-{#if mc.meta}
-	<div
-		class="absolute top-0 left-0 flex w-fit items-start justify-center gap-2 rounded bg-white p-2 shadow"
-	>
-		<div class="flex-col items-center justify-center">
-			<p class="text-lg font-bold underline">Air Monitors</p>
-			{#each Object.keys(mc.displayToggles) as label}
-				<label for={label} class="m-2 cursor-pointer whitespace-nowrap select-none">
-					<input
-						type="checkbox"
-						id={label}
-						name={label}
-						bind:checked={mc.displayToggles[label as keyof typeof mc.displayToggles]}
-					/>
-					{mc.displayOptions[label as keyof typeof mc.displayToggles]}
-				</label>
-			{/each}
-		</div>
-
-		<div class="flex-col items-center justify-center">
-			<p class="text-lg font-bold underline">Map Layers</p>
-			<label for="wind" class="m-2 cursor-pointer whitespace-nowrap select-none">
-				<input type="checkbox" id="wind" name="wind" bind:checked={wind.enabled} />
-				Wind
-			</label>
-		</div>
-	</div>
-{/if}
+<div
+	class="absolute top-0 left-0 flex w-fit items-start justify-center gap-2 rounded bg-white p-2 shadow"
+>
+	{@render children?.()}
+</div>
---
+++ src/lib/monitors/MonitorsDisplayOptions.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { MonitorsController } from "$lib/monitors/monitors.svelte";
+
+	const mc = new MonitorsController();
+</script>
+
+{#if mc.meta}
+  <div class="flex-col items-center justify-center">
+    <p class="text-lg font-bold underline">Air Monitors</p>
+    {#each Object.keys(mc.displayToggles) as label}
+      <label for={label} class="m-2 cursor-pointer whitespace-nowrap select-none">
+        <input
+          type="checkbox"
+          id={label}
+          name={label}
+          bind:checked={mc.displayToggles[label as keyof typeof mc.displayToggles]}
+        />
+        {mc.displayOptions[label as keyof typeof mc.displayToggles]}
+      </label>
+    {/each}
+  </div>
+{/if}
