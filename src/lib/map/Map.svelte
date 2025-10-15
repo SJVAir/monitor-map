f04acb0e59f009fdeed66c74ef7f9f4f864e6cc6
Alexander McCormick <alexandermccormick@protonmail.com>
2025-10-15 16:52:25 -0700

    Removes maptiles test

--- src/lib/map/Map.svelte
+++ src/lib/map/Map.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { onMount, onDestroy } from "svelte";
-	import type { GeoJSONSource } from "@maptiler/sdk";
 	import "@maptiler/sdk/dist/maptiler-sdk.css";
-	import { MapGeoJSONIntegration } from "./integrations.svelte.ts";
 	import { MapController, type MapConfig } from "./map.svelte.ts";
 
 	const { integrations }: Pick<MapConfig, "integrations"> = $props();
@@ -15,9 +13,6 @@
 			container,
 			integrations
 		});
-		setTimeout(async () => {
-			await mapCtrl.updateMapStyle("OPENSTREETMAP");
-		}, 1000 * 8);
 	});
 
 	onDestroy(() => mapCtrl.remove());
