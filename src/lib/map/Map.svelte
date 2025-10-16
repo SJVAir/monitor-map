27967bbcc32f5b86e02566692ebcea1f61fd6f61
Alexander McCormick <alexandermccormick@protonmail.com>
2025-10-16 10:47:35 -0700

    Converts map init to use attachments
    fixes bug where the element reference is undefined in the onMount
    lifecycle hook in production builds

--- src/lib/map/Map.svelte
+++ src/lib/map/Map.svelte
@@ -1,21 +1,18 @@
 <script lang="ts">
-	import { onMount, onDestroy } from "svelte";
+	import type { Attachment } from "svelte/attachments";
 	import "@maptiler/sdk/dist/maptiler-sdk.css";
 	import { MapController, type MapConfig } from "./map.svelte.ts";
 
 	const { integrations }: Pick<MapConfig, "integrations"> = $props();
 
 	const mapCtrl = new MapController();
-	let container: HTMLDivElement;
 
-	onMount(async () => {
+	const initMap: Attachment<HTMLDivElement> = (container: HTMLDivElement) => {
 		mapCtrl.init({
 			container,
 			integrations
 		});
-	});
-
-	onDestroy(() => mapCtrl.remove());
+	};
 </script>
 
-<div bind:this={container} style="width: 100%; height: 100%;"></div>
+<div {@attach initMap} style="width: 100%; height: 100%;"></div>
