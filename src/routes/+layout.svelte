26e518a7be5db66ec6040affa5cffbdc1ecf0ad7
Alexander McCormick <alexandermccormick@protonmail.com>
2025-10-16 10:48:01 -0700

    Imports Material Symbols

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,15 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+	/>
+	<script>
+		// Hack to force load Material Symbols early
+		document.fonts.load('10pt "Material Symbols Outlined"');
+	</script>
+</svelte:head>
+
 {@render children()}
