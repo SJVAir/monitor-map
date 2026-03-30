339cebf3fa9a480a03c430ef2d59ed028c149e7e
Alexander McCormick <alexandermccormick@protonmail.com>
2026-03-30 15:48:22 -0700

    Comments out old code

--- src/lib/loading/notice/load-notice.svelte.ts
+++ src/lib/loading/notice/load-notice.svelte.ts
@@ -1,39 +1,37 @@
-import { Singleton } from "@tstk/decorators";
-
-@Singleton
+// NOTE: This can probably be deleted
 export class LoadingQueue {
-  queue: Array<Symbol> = $state([]);
-  loading: boolean = $derived(this.queue.length > 0);
+	queue: Array<symbol> = $state([]);
+	loading: boolean = $derived(this.queue.length > 0);
 
-  add(id: Symbol) {
-    this.queue.push(id);
-  }
+	add(id: symbol) {
+		this.queue.push(id);
+	}
 
-  remove(id: Symbol) {
-    const idx = this.queue.indexOf(id);
-    this.queue.splice(idx, 1);
-  }
+	remove(id: symbol) {
+		const idx = this.queue.indexOf(id);
+		this.queue.splice(idx, 1);
+	}
 }
 
-export function TriggerLoadingScreen<
-  T extends (this: C, ...args: any[]) => any,
-  C
->(
-  originalMethod: T,
-  _context: ClassMethodDecoratorContext<C, T>
+export function TriggerLoadingScreen<T extends (this: C, ...args: any[]) => any, C>(
+	originalMethod: T,
+	_context: ClassMethodDecoratorContext<C, T>
 ): (this: C, ...args: Parameters<T>) => ReturnType<T> {
-  const queue = new LoadingQueue();
-  const id = Symbol();
+	const queue = new LoadingQueue();
+	const id = Symbol();
 
-  const replacementMethod: (this: C, ...args: Parameters<T>) => ReturnType<T> = function (this: C, ...args: Parameters<T>): ReturnType<T> {
-    queue.add(id);
-    const result = originalMethod.call(this, ...args);
-    if (result instanceof Promise) {
-      return result.finally(() => queue.remove(id)) as ReturnType<T>;
-    } else {
-      queue.remove(id);
-      return result;
-    }
-  };
-  return replacementMethod;
+	const replacementMethod: (this: C, ...args: Parameters<T>) => ReturnType<T> = function (
+		this: C,
+		...args: Parameters<T>
+	): ReturnType<T> {
+		queue.add(id);
+		const result = originalMethod.call(this, ...args);
+		if (result instanceof Promise) {
+			return result.finally(() => queue.remove(id)) as ReturnType<T>;
+		} else {
+			queue.remove(id);
+			return result;
+		}
+	};
+	return replacementMethod;
 }
