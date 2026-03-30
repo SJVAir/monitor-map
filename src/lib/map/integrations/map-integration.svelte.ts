3b8c64a60ad1f1cff9265486b24f7e64893e3375
Alexander McCormick <alexandermccormick@protonmail.com>
2026-03-30 15:53:10 -0700

    Removes singleton behavior

--- src/lib/map/integrations/map-integration.svelte.ts
+++ src/lib/map/integrations/map-integration.svelte.ts
@@ -1,32 +1,8 @@
-import { XMap } from "@tstk/builtin-extensions";
-
 export abstract class MapIntegration {
-  static instances: XMap<Function, MapIntegration> = new XMap();
+	abstract referenceId: string;
+	abstract enabled: boolean;
 
-  abstract referenceId: string;
-  abstract enabled: boolean;
+	abstract apply(): void;
 
-  constructor() {
-    if (MapIntegration.instances.has(this.constructor)) {
-      console.log("instance found for:", this);
-      return MapIntegration.instances.get(this.constructor);
-    }
-
-    console.log("setting instance:", this);
-    MapIntegration.instances.set(this.constructor, this);
-
-    //$effect(() => {
-    //  if (MapIntegration.mapCtrl.map) {
-    //    if (this.enabled) {
-    //      this.apply();
-    //    } else if (this.remove) {
-    //      this.remove();
-    //    }
-    //  }
-    //});
-  }
-
-  abstract apply(): void;
-
-  remove?(): void;
+	remove?(): void;
 }
