d47d894dd51cc799f368a1996bda905d0eee9c62
Alexander McCormick <alexandermccormick@protonmail.com>
2026-03-30 15:57:13 -0700

    Updates references to wind integration module

--- src/lib/wind/WindDisplayOptions.svelte
+++ src/lib/wind/WindDisplayOptions.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	import DisplayOption from "$lib/components/DisplayOption.svelte";
-	import { WindMapIntegration } from "./wind.svelte";
-
-	const wind = new WindMapIntegration();
+	import { windMapIntegration } from "./wind.svelte";
 </script>
 
 <DisplayOption>
 	<p class="text-lg font-bold whitespace-nowrap underline">Map Layers</p>
 	<label for="wind" class="m-2 cursor-pointer whitespace-nowrap select-none">
-		<input type="checkbox" id="wind" name="wind" bind:checked={wind.enabled} />
+		<input type="checkbox" id="wind" name="wind" bind:checked={windMapIntegration.enabled} />
 		Wind
 	</label>
 </DisplayOption>
