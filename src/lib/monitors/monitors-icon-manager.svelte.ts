e5b25f46f398ca22b8bfbb3d1f996f17428f04c0
Alexander McCormick <alexandermccormick@protonmail.com>
2026-03-30 15:54:51 -0700

    Corrects $effect logic

--- src/lib/monitors/monitors-icon-manager.svelte.ts
+++ src/lib/monitors/monitors-icon-manager.svelte.ts
@@ -1,7 +1,7 @@
 import type { MonitorData, SJVAirEntryLevel } from "@sjvair/sdk";
 import { asDataURI, circle, square, triangle } from "$lib/map/icons.ts";
 import { MapIconManager } from "$lib/map/integrations/map-icon-manager.ts";
-import { getCurrentLevels } from "./monitors.svelte.ts";
+import { monitorsManager } from "./monitors.svelte.ts";
 
 const MONITOR_ICONS = { circle, square, triangle };
 const MONITOR_ICON_WIDTH = 24;
@@ -10,101 +10,104 @@ const MONITOR_ICON_BORDER_WIDTH = 2;
 const MONITOR_ICON_DEFAULT_COLOR = "#969696"; // light gray
 
 export function getIconId<T extends MonitorData>(monitor: T, level: SJVAirEntryLevel): string {
-  const id = `${monitor.location}-${monitor.is_active ? level.name : "default"}`;
+	const id = `${monitor.location}-${monitor.is_active ? level.name : "default"}`;
 
-  switch (monitor.type) {
-    case "airgradient":
-      return `${id}-circle`;
+	switch (monitor.type) {
+		case "airgradient":
+			return `${id}-circle`;
 
-    case "airnow":
-    case "aqview":
-    case "bam1022":
-      return `${id}-triangle`;
+		case "airnow":
+		case "aqview":
+		case "bam1022":
+			return `${id}-triangle`;
 
-    case "purpleair":
-      return `${id}-${monitor.is_sjvair ? "circle" : "square"}`;
+		case "purpleair":
+			return `${id}-${monitor.is_sjvair ? "circle" : "square"}`;
 
-    default:
-      throw new Error(`Map icon for ${monitor.device} has not been set`);
-  }
+		default:
+			throw new Error(`Map icon for ${monitor.device} has not been set`);
+	}
 }
 
-export function genMonitorIcons() {
-  const levels = getCurrentLevels();
-  if (levels) {
-    const iconLevels = [...levels, { name: "default", color: MONITOR_ICON_DEFAULT_COLOR }];
+//export function genMonitorIcons() {
+//	const levels = monitorsManager.levels;
+//	if (levels) {
+//		const iconLevels = [...levels, { name: "default", color: MONITOR_ICON_DEFAULT_COLOR }];
+//
+//		for (const location of ["inside", "outside"]) {
+//			for (const [shapeName, shape] of Object.entries(MONITOR_ICONS)) {
+//				for (const level of iconLevels) {
+//					const id = `${location}-${level.name}-${shapeName}`;
+//					const src = asDataURI(
+//						shape(
+//							level.color,
+//							MONITOR_ICON_BORDER_WIDTH,
+//							location === "inside" ? "#000000" : undefined
+//						)
+//					);
+//
+//					if (this.icons.has(id)) {
+//						const icon = this.icons.get(id);
+//						if (icon.image.src === src) {
+//							continue;
+//						}
+//					} else {
+//						const icon = new Image();
+//						icon.src = src;
+//						icon.width = MONITOR_ICON_WIDTH;
+//						icon.height = MONITOR_ICON_HEIGHT;
+//
+//						this.register(id, icon);
+//					}
+//				}
+//			}
+//		}
+//	}
+//}
 
-    for (const location of ["inside", "outside"]) {
-      for (const [shapeName, shape] of Object.entries(MONITOR_ICONS)) {
-        for (const level of iconLevels) {
-          const id = `${location}-${level.name}-${shapeName}`;
-          const src = asDataURI(
-            shape(
-              level.color,
-              MONITOR_ICON_BORDER_WIDTH,
-              location === "inside" ? "#000000" : undefined
-            )
-          );
-
-          if (this.icons.has(id)) {
-            const icon = this.icons.get(id);
-            if (icon.image.src === src) {
-              continue;
-            }
-          } else {
-            const icon = new Image();
-            icon.src = src;
-            icon.width = MONITOR_ICON_WIDTH;
-            icon.height = MONITOR_ICON_HEIGHT;
-
-            this.register(id, icon);
-          }
-        }
-      }
-    }
-  }
-
-}
-
-// NOTE: Singleton used as mixing because an error is thrown when used as decorator
 export class MonitorsIconManager extends MapIconManager {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    $effect(() => {
-      const levels = getCurrentLevels();
-      if (levels) {
-        const iconLevels = [...levels, { name: "default", color: MONITOR_ICON_DEFAULT_COLOR }];
+		// TODO: decide if $effect should be kept, or this.icons should be converted to $derived
+		$effect.root(() => {
+			$effect(() => {
+				if (monitorsManager.levels) {
+					const iconLevels = [
+						...monitorsManager.levels,
+						{ name: "default", color: MONITOR_ICON_DEFAULT_COLOR }
+					];
 
-        for (const location of ["inside", "outside"]) {
-          for (const [shapeName, shape] of Object.entries(MONITOR_ICONS)) {
-            for (const level of iconLevels) {
-              const id = `${location}-${level.name}-${shapeName}`;
-              const src = asDataURI(
-                shape(
-                  level.color,
-                  MONITOR_ICON_BORDER_WIDTH,
-                  location === "inside" ? "#000000" : undefined
-                )
-              );
+					for (const location of ["inside", "outside"]) {
+						for (const [shapeName, shape] of Object.entries(MONITOR_ICONS)) {
+							for (const level of iconLevels) {
+								const id = `${location}-${level.name}-${shapeName}`;
+								const src = asDataURI(
+									shape(
+										level.color,
+										MONITOR_ICON_BORDER_WIDTH,
+										location === "inside" ? "#000000" : undefined
+									)
+								);
 
-              if (this.icons.has(id)) {
-                const icon = this.icons.get(id);
-                if (icon.image.src === src) {
-                  continue;
-                }
-              } else {
-                const icon = new Image();
-                icon.src = src;
-                icon.width = MONITOR_ICON_WIDTH;
-                icon.height = MONITOR_ICON_HEIGHT;
+								if (this.icons.has(id)) {
+									const icon = this.icons.get(id);
+									if (icon.image.src === src) {
+										continue;
+									}
+								} else {
+									const icon = new Image();
+									icon.src = src;
+									icon.width = MONITOR_ICON_WIDTH;
+									icon.height = MONITOR_ICON_HEIGHT;
 
-                this.register(id, icon);
-              }
-            }
-          }
-        }
-      }
-    });
-  }
+									this.register(id, icon);
+								}
+							}
+						}
+					}
+				}
+			});
+		});
+	}
 }
