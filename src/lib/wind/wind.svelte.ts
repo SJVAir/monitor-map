6bc93f7a0669ed9886822cf0b8399246a826769a
Alexander McCormick <alexandermccormick@protonmail.com>
2026-03-30 15:57:01 -0700

    Corrected state management and types for wind

--- src/lib/wind/wind.svelte.ts
+++ src/lib/wind/wind.svelte.ts
@@ -3,15 +3,18 @@ import { WindLayer } from "@maptiler/weather";
 import { BaseLayerSeperator } from "../map/integrations/base-layer-seperator.ts";
 import { MapLayerIntegration } from "$lib/map/integrations/map-layer-integration.svelte.ts";
 
-export class WindMapIntegration extends MapLayerIntegration {
-  referenceId: string = "MapTiler Wind";
+class WindMapIntegration extends MapLayerIntegration {
+	referenceId: string = "MapTiler Wind";
 
-  beforeLayer = new BaseLayerSeperator().referenceId;
+	beforeLayer = new BaseLayerSeperator().referenceId;
 
-  enabled: boolean = false;
+	enabled: boolean = $state(false);
 
-  mapLayer: Parameters<MaptilerMap["addLayer"]>[0] = new WindLayer({
-    id: this.referenceId,
-    opacity: 0.5,
-  });
+	mapLayer = new WindLayer({
+		id: this.referenceId,
+		opacity: 0.5
+	}) as unknown as Parameters<MaptilerMap["addLayer"]>[0];
 }
+
+export const windMapIntegration = new WindMapIntegration();
+export type { WindMapIntegration };
