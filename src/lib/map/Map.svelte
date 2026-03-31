cc6d827f2b4b4fec2b7f7870d04942f85ac23dda
Alexander McCormick <alexandermccormick@protonmail.com>
2026-03-31 15:44:46 -0700

    Updates references to integrationManager

--- src/lib/map/Map.svelte
+++ src/lib/map/Map.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
 	import "@maptiler/sdk/dist/maptiler-sdk.css";
 	import { initializeMap, mapManager } from "./map.svelte.ts";
-	import { IntegrationsManager } from "$lib/map/integrations/integrations-manager";
+	import { integrationsManager } from "$lib/map/integrations/integrations-manager";
 	import type { SomeMapIntegration } from "./integrations/types.ts";
-	//import { IntegrationsManager } from "./integrations/integrations-manager.ts";
 
 	const { integrations }: { integrations: Array<SomeMapIntegration> } = $props();
 	//const { integrations } = $props();
-
-	const integrationsManager = new IntegrationsManager();
 
 	integrationsManager.register(...integrations);
 	$effect(() => {
