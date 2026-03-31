51df6bc5e2ba7fd1a241b1ce26397d93f9d07005
Alexander McCormick <alexandermccormick@protonmail.com>
2026-03-31 15:41:27 -0700

    Adds collcoation sites to MonitorsManager

--- src/lib/monitors/monitors.svelte.ts
+++ src/lib/monitors/monitors.svelte.ts
@@ -1,7 +1,9 @@
 import {
+	getCollocationSites,
 	getMonitors,
 	getMonitorsLatest,
 	getMonitorsMeta,
+	type CollocationSite,
 	type MonitorData,
 	type MonitorLatestType,
 	type MonitorsMeta,
@@ -10,92 +12,16 @@ import {
 import { XMap } from "@tstk/builtin-extensions";
 import { Interval } from "@tstk/utils";
 
-//export class MonitorsController { }
-//interface MonitorsState {
-//	autoUpdate: Interval;
-//	//initialized: boolean;
-//	latest: Map<string, MonitorLatestType<"pm25" | "o3">>;
-//	list: Array<MonitorData> | null;
-//	meta: MonitorsMeta | null;
-//	pollutant: "pm25" | "o3" | null;
-//	displayOptions: object;
-//}
-//
-//export const monitorsState: MonitorsState = {
-//	autoUpdate: new Interval(async () => await update(), 2 * 60 * 1000),
-//	// the integration manager should keep track of what has been initialized
-//	//initialized: false,
-//	latest: new XMap(),
-//	list: $state(null),
-//	meta: $state(null),
-//	pollutant: $state(null),
-//	displayOptions: {
-//		purpleair: {
-//			label: "PurpleAir",
-//			value: $state(true)
-//		},
-//		sjvair: {
-//			label: "SJVAir non-FEM",
-//			value: $state(true)
-//		},
-//		aqview: {
-//			label: "AQview",
-//			value: $state(true)
-//		},
-//		bam1022: {
-//			label: "SJVAir FEM",
-//			value: $state(true)
-//		},
-//		airnow: {
-//			label: "AirNow",
-//			value: $state(true)
-//		},
-//		inactive: {
-//			label: "Inactive",
-//			value: $state(false)
-//		},
-//		inside: {
-//			label: "Inside",
-//			value: $state(false)
-//		}
-//	}
-//};
-//
-//const levels: Array<SJVAirEntryLevel> | null = $derived(
-//	monitorsState.meta?.entryType(monitorsState.pollutant ?? monitorsState.meta.default_pollutant)
-//		.asIter.levels || null
-//);
-//export function getCurrentLevels(): Array<SJVAirEntryLevel> | null {
-//	return levels;
-//}
-//
-//export async function init(): Promise<void> {
-//	if (monitorsState.initialized) return;
-//
-//	[monitorsState.meta, monitorsState.list] = await Promise.all([getMonitorsMeta(), getMonitors()]);
-//
-//	monitorsState.pollutant = monitorsState.meta.default_pollutant;
-//	monitorsState.latest = await getMonitorsLatestMap(monitorsState.pollutant);
-//	monitorsState.autoUpdate.start();
-//	monitorsState.initialized = true;
-//}
-//
-//export async function update(): Promise<void> {
-//	if (!monitorsState.initialized) return;
-//
-//	[monitorsState.list, monitorsState.latest] = await Promise.all([
-//		getMonitors(),
-//		getMonitorsLatestMap(monitorsState.pollutant || monitorsState.meta?.default_pollutant || "pm25")
-//	]);
-//}
-//
-
 class MonitorsManager {
 	autoUpdate: Interval = new Interval(async () => await this.update(), 2 * 60 * 1000);
 	initialized: boolean = $state(false);
+
+	collocationSites: Array<CollocationSite> | null = $state(null);
 	latest: XMap<string, MonitorLatestType<"pm25" | "o3">> | null = $state(null);
 	list: Array<MonitorData> | null = $state(null);
+
 	meta: MonitorsMeta | null = $state(null);
+
 	pollutant: "pm25" | "o3" | null = $state(null);
 
 	levels: Array<SJVAirEntryLevel> | null = $derived(
@@ -105,7 +31,12 @@ class MonitorsManager {
 	async init(): Promise<void> {
 		if (this.initialized) return;
 
-		[this.meta, this.list] = await Promise.all([getMonitorsMeta(), getMonitors()]);
+		//[this.meta, this.list] = await Promise.all([getMonitorsMeta(), getMonitors()]);
+		[this.collocationSites, this.list, this.meta] = await Promise.all([
+			getCollocationSites(),
+			getMonitors(),
+			getMonitorsMeta()
+		]);
 
 		this.pollutant = this.meta.default_pollutant;
 		this.latest = await getMonitorsLatestMap(this.pollutant);
@@ -116,7 +47,8 @@ class MonitorsManager {
 	async update(): Promise<void> {
 		if (!this.initialized) return;
 
-		[this.list, this.latest] = await Promise.all([
+		[this.collocationSites, this.list, this.latest] = await Promise.all([
+			getCollocationSites(),
 			getMonitors(),
 			getMonitorsLatestMap(this.pollutant || this.meta?.default_pollutant || "pm25")
 		]);
@@ -136,8 +68,5 @@ async function getMonitorsLatestMap(
 	return latest;
 }
 
-const monitorsManager = new MonitorsManager();
-await monitorsManager.init();
-
-export { monitorsManager };
+export const monitorsManager = new MonitorsManager();
 export type { MonitorsManager };
