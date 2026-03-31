74380cef6de8081b0c54f0a3a4e8078d61329dd2
Alexander McCormick <alexandermccormick@protonmail.com>
2026-03-31 15:46:55 -0700

    Adds DefaultMapStyle constant

--- src/lib/map/map.svelte.ts
+++ src/lib/map/map.svelte.ts
@@ -1,43 +1,17 @@
-import { config as MaptilerConfig, Map as MaptilerMap, MapStyle } from "@maptiler/sdk";
-//import { disable as disableLoadScreen } from "$lib/loading/screen/LoadScreen.svelte";
+import {
+	MapStyle,
+	MapStyleVariant,
+	config as MaptilerConfig,
+	Map as MaptilerMap
+} from "@maptiler/sdk";
 import type { Attachment } from "svelte/attachments";
 import { isGeoJSONSource } from "./utils";
 import type { Feature, GeoJsonProperties, Geometry } from "geojson";
 
 MaptilerConfig.apiKey = import.meta.env.VITE_MAPTILER_KEY;
 
+export const DefaultMapStyle: MapStyleVariant = MapStyle.STREETS_V4.DEFAULT;
 const excludeLayers = ["Sport", "Tourism", "Culture", "Shopping", "Food", "Transport"];
-
-//interface MapState {
-//	map: MaptilerMap | null;
-//}
-//export const mapState: MapState = $state({
-//	map: null
-//});
-
-export const initializeMap: Attachment<HTMLDivElement> = (container: string | HTMLElement) => {
-	const map = new MaptilerMap({
-		container: container,
-		center: [-119.7987626619462, 36.76272050981146],
-		zoom: 7,
-		style: MapStyle.STREETS,
-		projection: "globe",
-		space: {
-			preset: "milkyway"
-		}
-	});
-
-	map.on("load", async () => {
-		//map.once("idle", () => {
-		//  disableLoadScreen();
-		//});
-
-		//mapState.map = map;
-		mapManager.map = map;
-	});
-
-	return () => map.remove();
-};
 
 class MapManager {
 	map: MaptilerMap | null = $state(null);
@@ -75,3 +49,23 @@ class MapManager {
 
 export const mapManager = new MapManager();
 export type { MapManager };
+
+export const initializeMap: Attachment<HTMLDivElement> = (container: string | HTMLElement) => {
+	console.log("initialising map!");
+	const map = new MaptilerMap({
+		container: container,
+		center: [-119.7987626619462, 36.76272050981146],
+		zoom: 7,
+		style: DefaultMapStyle,
+		projection: "globe",
+		space: {
+			preset: "milkyway"
+		}
+	});
+
+	map.once("idle", () => {
+		mapManager.map = map;
+	});
+
+	return () => map.remove();
+};
