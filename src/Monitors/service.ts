415b357b097cda6332b6b719b5c23ca00f28f8ae
Alexander McCormick <alexandermccormick@protonmail.com>
2026-04-06 10:49:56 -0700

    Get primary pollutant from monitors metadata

--- src/Monitors/service.ts
+++ src/Monitors/service.ts
@@ -1,7 +1,11 @@
 import { ref } from "vue";
 import { useRoute } from "vue-router";
 import { asyncInitializer } from "../modules";
-import { getMonitorEntriesCSVUrl } from "../modules/api";
+import {
+  getMonitorEntriesCSVUrl,
+  MonitorsMeta,
+  getMonitorsMeta,
+} from "../modules/api";
 import { DateRange } from "../models";
 import * as MonitorsService from "./BackgroundRequests";
 import type { Ref } from "vue";
@@ -9,18 +13,19 @@ import type { Monitor } from "./Monitor";
 import type { MonitorId } from "../types";
 
 const monitorsLoadedEvent = new Event("MonitorsLoaded");
-export const primaryPollutant = ref<"pm25" | "o3">(getPrimaryPollutant());
+export const primaryPollutant = ref<"pm25" | "o3">("pm25");
 const monitors = ref<Record<string, Monitor>>({});
 const activeMonitor = ref<Monitor>();
-function getPrimaryPollutant(): "pm25" | "o3" {
-  const today = new Date();
-  const may = new Date(`05/01/${today.getUTCFullYear()}`);
-  const september = new Date(`09/30/${today.getUTCFullYear()}`);
-
-  //return (today > may && today < september) ? "o3" : "pm25";
-  // Overriding at Tim's request
-  return "pm25";
-}
+const monitorsMeta = ref<MonitorsMeta>();
+//function getPrimaryPollutant(): "pm25" | "o3" {
+//  const today = new Date();
+//  const may = new Date(`05/01/${today.getUTCFullYear()}`);
+//  const september = new Date(`09/30/${today.getUTCFullYear()}`);
+//
+//  //return (today > may && today < september) ? "o3" : "pm25";
+//  // Overriding at Tim's request
+//  return "pm25";
+//}
 
 interface MonitorsServiceExtras {
   activeMonitor: Ref<Monitor | undefined>;
@@ -35,6 +40,8 @@ export const useMonitorsService = asyncInitializer<MonitorsServiceModule>(
   async (resolve) => {
     const route = useRoute();
 
+    monitorsMeta.value = await getMonitorsMeta();
+    primaryPollutant.value = monitorsMeta.value.default_pollutant;
     await updateMonitors();
 
     if ("monitorId" in route.params) {
