be946b1c3f83026ebe141f3556a0020a5d0e2e58
Alexander McCormick <alexandermccormick@protonmail.com>
2026-04-21 13:21:06 -0700

    Converts marker cluster to ToggleSwitch

--- src/lib/monitors/MonitorsDisplayOptions.svelte
+++ src/lib/monitors/MonitorsDisplayOptions.svelte
@@ -1,19 +1,18 @@
 <script lang="ts">
 	import DisplayOption from "$lib/components/DisplayOption.svelte";
+	import ToggleSwitch from "$lib/components/ToggleSwitch.svelte";
 	import { monitorsMapIntegration } from "./monitors-map-integration.svelte";
 </script>
 
-	<DisplayOption>
-		<p class="text-lg font-bold underline">Air Monitors</p>
-		<label for="monitor-clusters" class="cursor-pointer whitespace-nowrap select-none">
-			<input
-				type="checkbox"
-				id="monitor-clusters"
-				name="monitor-clusters"
-				bind:checked={monitorsMapIntegration.clustered}
-			/>
-			Enable Clusters
-		</label>
+<DisplayOption>
+	<p class="text-lg font-bold underline">Air Monitors</p>
+	<div class="mb-2 text-xs">
+		<ToggleSwitch
+			id="monitor-clusters"
+			label="Marker Clusters"
+			bind:value={monitorsMapIntegration.clustered}
+		></ToggleSwitch>
+	</div>
 
 	{#each Object.entries(monitorsMapIntegration.displayOptions) as [id, option] (option.label)}
 		{@const inputId = `${id}-monitor-display-option`}
@@ -21,11 +20,5 @@
 			<input type="checkbox" id={inputId} name={option.label} bind:checked={option.value} />
 			{option.label}
 		</label>
-
-		{#each Object.values(monitorsMapIntegration.displayOptions) as option (option.label)}
-			<label for={option.label} class="cursor-pointer whitespace-nowrap select-none">
-				<input type="checkbox" id={option.label} name={option.label} bind:checked={option.value} />
-				{option.label}
-			</label>
-		{/each}
-	</DisplayOption>
+	{/each}
+</DisplayOption>
