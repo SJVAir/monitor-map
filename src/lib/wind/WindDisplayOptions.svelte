73e2f22051298c7bc0f3959b7c4f37722eed8731
Alexander McCormick <alexandermccormick@protonmail.com>
2026-04-21 13:21:22 -0700

    Converts Wind display option to ToggleSwitch

--- src/lib/wind/WindDisplayOptions.svelte
+++ src/lib/wind/WindDisplayOptions.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import DisplayOption from "$lib/components/DisplayOption.svelte";
+	import ToggleSwitch from "$lib/components/ToggleSwitch.svelte";
 	import { windMapIntegration } from "./wind.svelte";
 </script>
 
 <DisplayOption>
 	<p class="text-lg font-bold whitespace-nowrap underline">Map Layers</p>
-	<label for="wind" class="m-2 cursor-pointer whitespace-nowrap select-none">
-		<input type="checkbox" id="wind" name="wind" bind:checked={windMapIntegration.enabled} />
-		Wind
-	</label>
+	<ToggleSwitch id="wind" label="Wind" bind:value={windMapIntegration.enabled}></ToggleSwitch>
 </DisplayOption>
