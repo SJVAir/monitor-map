1f1e81f7528c99215a744ca36d032f8f7601bbdb
Alexander McCormick <alexandermccormick@protonmail.com>
2026-04-23 13:20:43 -0700

    Creates CollocationIconManager

--- src/lib/collocation-sites/collocations-icon-manager.svelte.ts
+++ src/lib/collocation-sites/collocations-icon-manager.svelte.ts
@@ -1,81 +1,21 @@
-import type { MonitorData, SJVAirEntryLevel } from "@sjvair/sdk";
-import { asDataURI, circle, square, triangle } from "$lib/map/icons.ts";
-import { MapIconManager } from "$lib/map/integrations/map-icon-manager.ts";
-import { mapManager } from "$lib/map/map.svelte.ts";
-import { monitorsManager } from "$lib/monitors/monitors.svelte";
+import { CROSSHAIR_ICON_SIZE, withCrosshair } from "$lib/map/icons.ts";
+import { MonitorShapeIconManager } from "$lib/monitors/monitors-icon-manager.svelte.ts";
 
-const MONITOR_ICONS = { circle, square, triangle };
-const MONITOR_ICON_WIDTH = 24;
-const MONITOR_ICON_HEIGHT = 24;
-const MONITOR_ICON_BORDER_WIDTH = 2;
-const MONITOR_ICON_DEFAULT_COLOR = "#969696"; // light gray
+const COLLOCATION_ICON_PREFIX = "collocation";
+const CROSSHAIR_COLOR = "#4A5FC6";
 
-export function getIconId<T extends MonitorData>(monitor: T, level: SJVAirEntryLevel): string {
-	const id = `${monitor.location}-${monitor.is_active ? level.name : "default"}`;
-
-	switch (monitor.type) {
-		case "airgradient":
-			return `${id}-circle`;
-
-		case "airnow":
-		case "aqview":
-		case "bam1022":
-			return `${id}-triangle`;
-
-		case "purpleair":
-			return `${id}-${monitor.is_sjvair ? "circle" : "square"}`;
-
-		default:
-			throw new Error(`Map icon for ${monitor.device} has not been set`);
-	}
+export function getCollocationIconId(baseIconId: string): string {
+	return `${COLLOCATION_ICON_PREFIX}-${baseIconId}`;
 }
 
-export class MonitorsIconManager extends MapIconManager {
-	constructor() {
-		super();
-
-		$effect.root(() => {
-			$effect(() => {
-				if (monitorsManager.levels) {
-					const iconLevels = [
-						...monitorsManager.levels,
-						{ name: "default", color: MONITOR_ICON_DEFAULT_COLOR }
-					];
-
-					for (const location of ["inside", "outside"]) {
-						for (const [shapeName, shape] of Object.entries(MONITOR_ICONS)) {
-							for (const level of iconLevels) {
-								const id = `${location}-${level.name}-${shapeName}`;
-								const src = asDataURI(
-									shape(
-										level.color,
-										MONITOR_ICON_BORDER_WIDTH,
-										location === "inside" ? "#000000" : undefined
-									)
-								);
-
-								if (this.icons.has(id)) {
-									const icon = this.icons.get(id);
-									if (icon.image.src !== src) {
-										icon.image.src = src;
-										if (mapManager.map?.hasImage(id)) {
-											this.updateImage(icon, mapManager.map);
-										}
-									}
-									continue;
-								} else {
-									const icon = new Image();
-									icon.src = src;
-									icon.width = MONITOR_ICON_WIDTH;
-									icon.height = MONITOR_ICON_HEIGHT;
-
-									this.register(id, icon);
-								}
-							}
-						}
-					}
-				}
-			});
-		});
+export class CollocationIconManager extends MonitorShapeIconManager {
+	protected get idPrefix(): string {
+		return COLLOCATION_ICON_PREFIX;
+	}
+	protected get iconSize(): number {
+		return CROSSHAIR_ICON_SIZE;
+	}
+	protected transformSvg(svg: string): string {
+		return withCrosshair(svg, CROSSHAIR_COLOR);
 	}
 }
