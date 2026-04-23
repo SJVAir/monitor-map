98c6db7a8ddea1fca439d21a23bbb307ebae7318
Alexander McCormick <alexandermccormick@protonmail.com>
2026-04-22 18:04:52 -0700

    Replaces WindDisplayOptions with MapLayerDisplayOptions

--- src/lib/components/MapLayersDisplayOptions.svelte
+++ src/lib/components/MapLayersDisplayOptions.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	import DisplayOption from "$lib/components/DisplayOption.svelte";
 	import ToggleSwitch from "$lib/components/ToggleSwitch.svelte";
-	import { windMapIntegration } from "./wind.svelte";
+	import { windMapIntegration } from "$lib/wind/wind.svelte";
+	import { collocationSitesMapIntegration } from "$lib/collocation-sites/collocations-map-integration.svelte";
 </script>
 
 <DisplayOption>
 	<p class="text-lg font-bold whitespace-nowrap underline">Map Layers</p>
 	<ToggleSwitch id="wind" label="Wind" bind:value={windMapIntegration.enabled}></ToggleSwitch>
+	<ToggleSwitch
+		id="collocation-sites"
+		label="Collocation Sites"
+		bind:value={collocationSitesMapIntegration.enabled}
+	></ToggleSwitch>
 </DisplayOption>
