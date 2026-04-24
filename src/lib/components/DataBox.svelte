400a4d84a0d933c3de1807affb5053ccdd5c4201
Alexander McCormick <alexandermccormick@protonmail.com>
2026-04-23 17:25:46 -0700

    Creates DataBox component

---
+++ src/lib/components/DataBox.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { darken } from "color2k";
+
+	interface DataBoxProps {
+		color: string;
+		header: string;
+		subheading?: string;
+		value: string;
+	}
+
+	const { color, header, subheading, value }: DataBoxProps = $props();
+</script>
+
+<div
+	class="inline-block rounded-sm border-4 p-1 text-center"
+	style:background-color={color}
+	style:border-color={darken(color, 0.1)}
+>
+	<p class="text-base whitespace-nowrap">{header}</p>
+	<p class="text-3xl font-bold whitespace-nowrap">{value}</p>
+	{#if subheading}
+		<p class="whitespace-nowrap">{subheading}</p>
+	{/if}
+</div>
