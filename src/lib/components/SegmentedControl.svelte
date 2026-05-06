51cd13e4518e2e1a1d817b47d4d6c7a60c40092d
Alexander McCormick <alexandermccormick@protonmail.com>
2026-05-06 12:30:58 -0700

    Creates initial SegmentedControl component and pollutant swapping

---
+++ src/lib/components/SegmentedControl.svelte
@@ -0,0 +1,103 @@
+<script lang="ts">
+	//import type { Attachment } from "svelte/attachments";
+
+	interface SegmentedControlProps {
+		segmentLabel?: string;
+		options: Array<{ label: string; value: string }>;
+		group: string;
+	}
+
+	let { segmentLabel: label, options, group = $bindable() }: SegmentedControlProps = $props();
+
+	//const animationTrigger: Attachment<HTMLDivElement> = (element) => {
+
+	//};
+	let inner: HTMLDivElement | undefined = $state();
+
+	$effect(() => {
+		void group;
+		if (!inner) return;
+
+		const checked = inner.querySelector<HTMLInputElement>("input:checked");
+		if (!checked) return;
+
+		const padding = 16;
+		const label = checked.closest("label") as HTMLLabelElement;
+		const innerRect = inner.getBoundingClientRect();
+		const labelRect = label.getBoundingClientRect();
+
+		inner.style.setProperty("--highlight-width", `${labelRect.width + padding}px`);
+		inner.style.setProperty(
+			"--highlight-x-pos",
+			`${labelRect.left - innerRect.left - padding / 2}px`
+		);
+	});
+</script>
+
+<div class="relative w-full">
+	{#if label}
+		<p class="text-lg font-bold underline">{label}</p>
+	{/if}
+	<div
+		bind:this={inner}
+		class="relative flex justify-between before:absolute before:top-0.5 before:bottom-0.5 before:left-0 before:z-0 before:w-(--highlight-width) before:translate-x-(--highlight-x-pos) before:rounded-md before:bg-[#5465FF] before:transition-transform before:duration-500 before:content-['']"
+	>
+		{#each options as { label, value } (value)}
+			{@const inputId = `${value}-segmented-control-option`}
+			<!--
+			<input class="peer hidden" type="radio" id={inputId} name={label} {value} bind:group />
+			<label
+				for={inputId}
+				class="peer-checked/text-white cursor-pointer whitespace-nowrap select-none"
+			>
+				{label}
+			</label>
+			<input type="radio" name={label} id={inputId} class="peer hidden" {value} bind:group />
+			<label
+				for={inputId}
+				class="transition-all peer-checked:border-blue-600 peer-checked:bg-blue-600 peer-checked:text-white active:scale-95"
+			>
+				{label}
+			</label>
+      -->
+			<!-- This is probably what we're using
+			<label
+				for={inputId}
+				class="block cursor-pointer rounded-md border px-4 py-2 transition-all active:scale-95 has-[:checked]:border-green-600 has-[:checked]:bg-green-600 has-[:checked]:text-white"
+			>
+				<input type="radio" id={inputId} name={label} {value} bind:group class="hidden" />
+				{label}
+			</label>
+      -->
+			<label
+				for={inputId}
+				class="relative z-1 cursor-pointer text-center transition-all active:scale-95 has-[:checked]:text-white"
+			>
+				<input type="radio" id={inputId} name={label} {value} bind:group class="hidden" />
+				{label}
+			</label>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.controls-container {
+		--highlight-width: auto;
+		--highlight-x-pos: 0;
+
+		display: flex;
+	}
+
+	.controls {
+		display: inline-flex;
+		justify-content: space-between;
+		background: white;
+		box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+		border-radius: 10px;
+		max-width: 500px;
+		padding: 12px;
+		margin: auto;
+		overflow: hidden;
+		position: relative;
+	}
+</style>
--- src/lib/monitors/MonitorsDisplayOptions.svelte
+++ src/lib/monitors/MonitorsDisplayOptions.svelte
@@ -1,11 +1,41 @@
 <script lang="ts">
+	import type { MonitorsMeta } from "@sjvair/sdk";
 	import DisplayOption from "$lib/components/DisplayOption.svelte";
+	import SegmentedControl from "$lib/components/SegmentedControl.svelte";
 	import ToggleSwitch from "$lib/components/ToggleSwitch.svelte";
+	import { monitorsManager } from "./monitors.svelte";
 	import { monitorsMapIntegration } from "./monitors-map-integration.svelte";
+
+	interface PollutantDisplayOptions {
+		label: string;
+		value: MonitorsMeta["default_pollutant"];
+	}
+
+	const pollutants: Array<PollutantDisplayOptions> = [
+		{
+			label: "PM 2.5",
+			value: "pm25"
+		},
+		{
+			label: "Ozone",
+			value: "o3"
+		}
+	];
+
+	$effect(() => {
+		monitorsManager.list = [];
+		monitorsManager.latest = null;
+		monitorsManager.update();
+	});
 </script>
 
 <DisplayOption>
 	<p class="text-lg font-bold underline">Air Monitors</p>
+	{#if monitorsManager.pollutant}
+		<div class="p-1">
+			<SegmentedControl options={pollutants} bind:group={monitorsManager.pollutant} />
+		</div>
+	{/if}
 	<div class="mb-2 text-xs">
 		<ToggleSwitch
 			id="monitor-clusters"
