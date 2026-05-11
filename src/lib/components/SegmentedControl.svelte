167e258e9d8fb1ebd0ba697b0432ab922a3053bb
Alexander McCormick <alexandermccormick@protonmail.com>
2026-05-11 13:10:37 -0700

    Removes hardcoded width

--- src/lib/components/SegmentedControl.svelte
+++ src/lib/components/SegmentedControl.svelte
@@ -35,44 +35,19 @@
 	});
 </script>
 
-<div class="relative w-full">
+<div class="relative">
 	{#if label}
 		<p>{label}</p>
 	{/if}
 	<div
 		bind:this={inner}
-		class="relative flex justify-between px-[.5em] before:absolute before:top-[0.125em] before:bottom-[0.125em] before:left-0 before:z-0 before:w-(--highlight-width) before:translate-x-(--highlight-x-pos) before:rounded-md before:bg-[#5465FF] before:transition-transform before:duration-500 before:content-['']"
+		class="relative flex gap-4 px-[.5em] before:absolute before:top-[0.125em] before:bottom-[0.125em] before:left-0 before:z-0 before:w-(--highlight-width) before:translate-x-(--highlight-x-pos) before:rounded-md before:bg-[#5465FF] before:transition-transform before:duration-500 before:content-['']"
 	>
 		{#each options as { label, value } (value)}
 			{@const inputId = `${value}-segmented-control-option`}
-			<!--
-			<input class="peer hidden" type="radio" id={inputId} name={label} {value} bind:group />
 			<label
 				for={inputId}
-				class="peer-checked/text-white cursor-pointer whitespace-nowrap select-none"
-			>
-				{label}
-			</label>
-			<input type="radio" name={label} id={inputId} class="peer hidden" {value} bind:group />
-			<label
-				for={inputId}
-				class="transition-all peer-checked:border-blue-600 peer-checked:bg-blue-600 peer-checked:text-white active:scale-95"
-			>
-				{label}
-			</label>
-      -->
-			<!-- This is probably what we're using
-			<label
-				for={inputId}
-				class="block cursor-pointer rounded-md border px-4 py-2 transition-all active:scale-95 has-[:checked]:border-green-600 has-[:checked]:bg-green-600 has-[:checked]:text-white"
-			>
-				<input type="radio" id={inputId} name={label} {value} bind:group class="hidden" />
-				{label}
-			</label>
-      -->
-			<label
-				for={inputId}
-				class="relative z-1 cursor-pointer text-center transition-all active:scale-95 has-[:checked]:text-white"
+				class="relative z-1 cursor-pointer text-center transition-all duration-500 active:scale-95 has-checked:text-white"
 			>
 				<input type="radio" id={inputId} name={label} {value} bind:group class="hidden" />
 				{label}
@@ -80,25 +55,3 @@
 		{/each}
 	</div>
 </div>
-
-<style>
-	.controls-container {
-		--highlight-width: auto;
-		--highlight-x-pos: 0;
-
-		display: flex;
-	}
-
-	.controls {
-		display: inline-flex;
-		justify-content: space-between;
-		background: white;
-		box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-		border-radius: 10px;
-		max-width: 500px;
-		padding: 12px;
-		margin: auto;
-		overflow: hidden;
-		position: relative;
-	}
-</style>
