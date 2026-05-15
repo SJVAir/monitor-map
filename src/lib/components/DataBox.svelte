d5d5feb25e327078946a0f4bf2f15b5c7731386a
Alexander McCormick <alexandermccormick@protonmail.com>
2026-05-14 17:34:20 -0700

    Better responsiveness

--- src/lib/components/DataBox.svelte
+++ src/lib/components/DataBox.svelte
@@ -14,14 +14,16 @@
 </script>
 
 <div
-	class="inline-block rounded-sm border-4 p-1 text-center"
+	class="flex flex-col rounded-sm border-4 p-1 text-center"
 	style:background-color={color}
 	style:border-color={borderColor}
 	style:color={textColor}
 >
 	<p class="text-base whitespace-nowrap">{header}</p>
-	<p class="text-3xl font-bold whitespace-nowrap">{value}</p>
-	{#if subheading}
-		<p class="whitespace-nowrap">{subheading}</p>
-	{/if}
+	<div class="flex flex-1 flex-col items-center justify-center">
+		<p class={["font-bold whitespace-nowrap", subheading ? "text-3xl" : "text-4xl"]}>{value}</p>
+		{#if subheading}
+			<p class="whitespace-nowrap">{subheading}</p>
+		{/if}
+	</div>
 </div>
