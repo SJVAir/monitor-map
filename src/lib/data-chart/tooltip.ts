c8387ca5d9428176f702f2ba9b7fbbca0838468f
Alexander McCormick <alexandermccormick@protonmail.com>
2026-05-20 12:08:31 -0700

    Fix double-# color bug: strip existing # from level.color in getColors()

--- src/lib/data-chart/tooltip.ts
+++ src/lib/data-chart/tooltip.ts
@@ -2,7 +2,7 @@ import { format, fromUnixTime } from "date-fns";
 import { monitorsManager } from "../monitors/monitors.svelte";
 
 function getColors(): Array<[number, string]> {
-	return (monitorsManager.levels ?? []).map((level) => [level.range[0], level.color]) as Array<
+	return (monitorsManager.levels ?? []).map((level) => [level.range[0], level.color.replace(/^#/, "")]) as Array<
 		[number, string]
 	>;
 }
