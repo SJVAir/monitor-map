c62dd091db211ed7c27c5c273ce489ea972faee2
Alexander McCormick <alexandermccormick@protonmail.com>
2026-05-20 11:33:11 -0700

    Add DataChartManager reactive state class

---
+++ src/lib/data-chart/data-chart.svelte.ts
@@ -0,0 +1,10 @@
+import { createDateRange } from "./DateRange.ts";
+import type { DateRange } from "./DateRange.ts";
+
+class DataChartManager {
+	dateRange: DateRange = $state(createDateRange());
+	chartData: uPlot.AlignedData = $state([]);
+	loading: boolean = $state(false);
+}
+
+export type { DataChartManager };
