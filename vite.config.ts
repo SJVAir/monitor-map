abe0cd25d9694b8a90de731369337c53ef835eab
Alexander McCormick <alexandermccormick@protonmail.com>
2026-06-01 12:39:02 -0700

    Fix: corrects vite config for exporting final map package

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,29 @@ import { enhancedImages } from "@sveltejs/enhanced-img";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { fileURLToPath, URL } from "node:url";
 
-export default defineConfig({
-	plugins: [tailwindcss(), enhancedImages(), svelte()],
-	resolve: {
-		alias: {
-			$lib: fileURLToPath(new URL("./src/lib", import.meta.url))
+export default defineConfig(({ mode }) => {
+	const devMode = mode === "development";
+
+	return {
+		base: devMode ? "/" : "/static/monitor-map/",
+		plugins: [tailwindcss(), enhancedImages(), svelte()],
+		resolve: {
+			alias: {
+				$lib: fileURLToPath(new URL("./src/lib", import.meta.url))
+			}
+		},
+		build: {
+			target: "es2024",
+			outDir: fileURLToPath(new URL("./dist", import.meta.url)),
+			rolldownOptions: {
+				input: {
+					sjvairMonitorMap: fileURLToPath(new URL("./src/main.ts", import.meta.url))
+				},
+				output: {
+					entryFileNames: "[name].js",
+					assetFileNames: "[name].[ext]"
+				}
+			}
 		}
-	},
-	build: {
-		target: "es2024"
-	}
+	};
 });
